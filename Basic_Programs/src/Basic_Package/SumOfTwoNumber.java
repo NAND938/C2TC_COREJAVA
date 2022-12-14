a8b45fdf1adff354b45f831dd01b8ab1710c4844
NAND KISHOR SINGH <NAND KISHOR SINGH@DESKTOP-K754PNQ>
2022-12-15 05:09:57 +0530

    Forth

---
+++ Basic_Programs/src/Basic_Package/SumOfTwoNumber.java
@@ -0,0 +1,11 @@
+package Basic_Package;
+
+public class SumOfTwoNumber {
+
+	public static void main(String[] args) 
+	{
+		int a=5,b=10,c=a+b;
+		System.out.println("The sum of two numbers is"+c);
+	}
+
+}
